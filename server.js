23febe8a38f574e45fcaa5f79f9bd16dd09b5220
Jayed Apu <95185551+apujayed@users.noreply.github.com>
2024-08-22 17:11:47 +0600

    new

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const request = require('request');
-const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.use(cors()); 
+
 // Middleware to add CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -12,55 +12,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// Predefined routes array
-const routes = [
-    { "id": 1, "from": "B Sirajul Islam", "to": "Dhaka" },
-    { "id": 4, "from": "Thakurgaon_Road", "to": "Dhaka" },
-    { "id": 5, "from": "Pirganj", "to": "Dhaka" },
-    { "id": 6, "from": "Setabganj", "to": "Dhaka" },
-    { "id": 7, "from": "Dinajpur", "to": "Dhaka" },
-    { "id": 8, "from": "Chirirbandar", "to": "Dhaka" },
-    { "id": 9, "from": "Parbatipur", "to": "Dhaka" },
-    { "id": 10, "from": "Fulbari", "to": "Dhaka" },
-    { "id": 11, "from": "Birampur", "to": "Dhaka" },
-    { "id": 12, "from": "Panchbibi", "to": "Dhaka" },
-    { "id": 13, "from": "Joypurhat", "to": "Dhaka" },
-    { "id": 14, "from": "Akkelpur", "to": "Dhaka" },
-    { "id": 15, "from": "Santahar", "to": "Dhaka" },
-    { "id": 16, "from": "Ahsanganj", "to": "Dhaka" },
-    { "id": 17, "from": "Natore", "to": "Dhaka" }
-];
-
-// Proxy route to fetch data for all routes and send the results
+// Proxy route
 app.use('/', (req, res) => {
-
-    const { date_of_journey } = req.query; // Get the date from the query parameters
-
-    if (!date_of_journey) {
-        return res.status(400).json({ error: 'Missing date_of_journey parameter' });
-    }
-
-    let results = [];
-    let completedRequests = 0;
-
-    routes.forEach((route, index) => {
-        const url = `https://railspaapi.shohoz.com/v1.0/web/bookings/search-trips-v2?from_city=${encodeURIComponent(route.from)}&to_city=${encodeURIComponent(route.to)}&date_of_journey=${encodeURIComponent(date_of_journey)}&seat_class=AC_B`;
-
-        request(url, (error, response, body) => {
-            console.log(response);
-            
-            if (!error && response.statusCode === 200) {
-                results.push({ route_id: route.id, data: JSON.parse(body) });
-            } else {
-                results.push({ route_id: route.id, error: 'Failed to fetch data' });
-            }
-
-            completedRequests++;
-            if (completedRequests === routes.length) {
-                res.json(results); // Send the combined results when all requests are completed
-            }
-        });
-    });
+    const url = 'https://railspaapi.shohoz.com/v1.0/app/bookings/search-trips-v2?from_city=B%20Sirajul%20Islam&to_city=Dhaka&date_of_journey=23-Aug-2024&seat_class=AC_B'; // Replace with your API endpoint
+    req.pipe(request(url)).pipe(res);
 });
 
 // Start the server
