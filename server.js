f5259687ae7137dfa3a731f9e4734b03df1341ca
Jayed Apu <95185551+apujayed@users.noreply.github.com>
2024-08-22 16:57:27 +0600

    cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors()); 
